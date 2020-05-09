1ec552bc69c866062ee08c86c2d3df8914160896
antodasil <dasil.anto@gmail.com>
2020-05-09 09:58:48 +0200

    Use Configuration in property class

--- src/Property.ts
+++ src/Property.ts
@@ -1,18 +1,17 @@
 import * as vscode from 'vscode';
+import Configuration from './Configuration';
 
 export default class Property {
     private name: string;
     private type: string;
 
-    private config: vscode.WorkspaceConfiguration;
-    private editorConfig: vscode.WorkspaceConfiguration;
+    private config: Configuration;
 
     public constructor(name: string, type: string) {
         this.name = name;
         this.type = type;
 
-        this.config = vscode.workspace.getConfiguration('php-code-generator');
-        this.editorConfig = vscode.workspace.getConfiguration('editor');
+        this.config = Configuration.getInstance();
     }
 
     public getName() {
@@ -26,7 +25,7 @@ export default class Property {
     public generateGetter(): string {
 
         let eol = vscode.window.activeTextEditor?.document.eol === 1 ? '\n' : '\r\n';
-        let tab = this.getTab();
+        let tab = this.config.getTab();
         let beforeBracket = this.config.get('newLineBeforeBracket') === true ? eol + tab : ' ';
 
         let name: string = this.getName();
@@ -35,7 +34,6 @@ export default class Property {
         let returnType = this.getType() === '' ? '' : ': ' + this.getType();
         let commentType = this.getType() === '' ? '' : eol + tab + '* @return ' + this.getType();
         return `
-
 ${tab}/**
 ${tab}* Get $${name}${commentType}
 ${tab}*/
@@ -47,7 +45,7 @@ ${tab}}`;
     public generateSetter(): string {
 
         let eol = vscode.window.activeTextEditor?.document.eol === 1 ? '\n' : '\r\n';
-        let tab = this.getTab();
+        let tab = this.config.getTab();
         let beforeBracket = this.config.get('newLineBeforeBracket') === true ? eol + tab : ' ';
 
         let name: string = this.getName();
@@ -56,20 +54,11 @@ ${tab}}`;
         let type = this.getType() + " ";
         let commentParam = this.getType() === '' ? '' : eol + tab + '* @param ' + this.getType() + ' $' + name; 
         return `
-
 ${tab}/**
 ${tab}* Set $${name}${commentParam}
 ${tab}*/
 ${tab}public function set${nameCaps}(${type}$${name})${beforeBracket}{
 ${tab}${tab}$this->${name} = $${name};
 ${tab}}`;
-    }
-
-    private getTab(): string {
-        let tab = '';
-        for(let i = 0; i < Number(this.editorConfig.get('tabSize')); i++) {
-            tab += ' ';
-        }
-        return tab;
     }
 }
