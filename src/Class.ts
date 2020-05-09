0444361a6ac8546767a78a1736be3fb12b0d3c76
antodasil <dasil.anto@gmail.com>
2020-05-09 09:58:34 +0200

    Add Class class

---
+++ src/Class.ts
@@ -0,0 +1,35 @@
+import * as vscode from 'vscode';
+import Property from './Property';
+import Configuration from './Configuration';
+
+export default class Class {
+
+    private properties: Array<Property>;
+
+    private config: Configuration;
+
+    public constructor(properties: Array<Property>) {
+        this.properties = properties;
+
+        this.config = Configuration.getInstance();
+    }
+
+    public generateConstruct(): string {
+
+        let eol = vscode.window.activeTextEditor?.document.eol === 1 ? '\n' : '\r\n';
+        let tab = this.config.getTab();
+        let beforeBracket = this.config.get('newLineBeforeBracket') === true ? eol + tab : ' ';
+        return `
+${tab}public function __construct()${beforeBracket}{
+${tab}${tab}
+${tab}}`;
+    }
+
+    public generateGetters(): string {
+        return '';
+    }
+
+    public generateSetters(): string {
+        return '';
+    }
+}
