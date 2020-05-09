9e50b8af2c72221807600cb531b4ee896c39cff1
antodasil <dasil.anto@gmail.com>
2020-05-09 09:57:56 +0200

    Add configuration class

---
+++ src/Configuration.ts
@@ -0,0 +1,35 @@
+import * as vscode from 'vscode';
+
+export default class Configuration {
+
+    private static instance: Configuration;
+
+    private config: vscode.WorkspaceConfiguration;
+    private editorConfig: vscode.WorkspaceConfiguration;
+
+    private constructor() {
+
+        this.config = vscode.workspace.getConfiguration('php-code-generator');
+        this.editorConfig = vscode.workspace.getConfiguration('editor');
+    }
+
+    public static getInstance() {
+        if(!this.instance) {
+            this.instance = new Configuration();
+        }
+
+        return this.instance;
+    }
+
+    public get(property: string) {
+        return this.config.get(property) || this.editorConfig.get(property);
+    }
+
+    public getTab(): string {
+        let tab = '';
+        for(let i = 0; i < Number(this.get('tabSize')); i++) {
+            tab += ' ';
+        }
+        return tab;
+    }
+}
